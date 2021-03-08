b11317b60ae522ac1b87463170a0a005b2c6ff5e
Giuliana Martinatti Giorjiani <32231070+Gmgiorjiani@users.noreply.github.com>
2021-03-08 15:51:46 +0000

    Update artBrain.m

--- artBrain.m
+++ artBrain.m
@@ -1,7 +1,8 @@
-% Turn your brain structural (T1.nii) image files into colorful contours :)
+% Turn your own brain structural (T1.nii) image files into colorful contours :)
+% Otherwise you can download the SPM toolbox (Friston, K. J. et al., 1994) and use their ~/Tamplate/T1.nii.
 
 vol = load_nii('~/T1.nii'); % read your T1 volume image here
-X = flip(squeeze(vol.img(100,:,:)))'; % slect the slice for the 1st view
+X = squeeze(vol.img(100,:,:)))'; % slect the slice for the 1st view
 Y = squeeze(vol.img(:,100,:))'; % slect the slice for the 2nd view
 Z = squeeze(vol.img(:,:,100))'; % slect the slice for the 3rd view
 
